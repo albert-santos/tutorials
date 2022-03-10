8366efdeb6f5bfc1723b195381dffb8d32326296
Albert Santos <alberteinsteincds@gmail.com>
2022-03-10 12:31:52 -0300

    Primeiro programa em C++

---
+++ Cpp/freecodecamp-course/3.FirstSteps/3.2FirstCppProgram/README.md
@@ -0,0 +1,5 @@
+## Primeiro programa em C++
+- Copiar template da aula anterior
+- Executar tarefa (terminal > executar tarefa)
+- Construir tarefa (Terminal > Build tarefa)
+- executar o roost.exe
---
+++ Cpp/freecodecamp-course/3.FirstSteps/3.2FirstCppProgram/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Biblioteca para realizar entradas e saídas
+
+// Função main é por onde o código em c++ inicia
+int main()
+{   
+    //endl imprime uma nova linha
+    std::cout << "Hello World!" << std::endl;
+}
