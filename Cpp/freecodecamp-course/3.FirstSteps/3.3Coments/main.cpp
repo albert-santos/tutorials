152144d0313b1f695a4bfd32dbbf84b6d219d520
Albert Santos <alberteinsteincds@gmail.com>
2022-03-10 12:41:58 -0300

    comentários em c++

---
+++ Cpp/freecodecamp-course/3.FirstSteps/3.3Coments/main.cpp
@@ -0,0 +1,15 @@
+// Importa a biblioteca iostream
+#include <iostream>
+
+/*
+    Esse é um bloco de comentátio
+    Várias linhas podem ser colocadas aqui
+*/
+
+int main()
+{   
+    // Imprime a mensagem no console
+    std::cout <<"Hello World" << std::endl;
+    return 0;
+    
+}
