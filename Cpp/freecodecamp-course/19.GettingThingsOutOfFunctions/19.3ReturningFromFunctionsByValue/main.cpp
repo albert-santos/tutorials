8e54737b2df77fd6a9dbd2b87f75fab4551394d8
Albert Santos <alberteinsteincds@gmail.com>
2022-03-22 13:34:01 -0300

    Returning From Functions by Value

---
+++ Cpp/freecodecamp-course/19.GettingThingsOutOfFunctions/19.3ReturningFromFunctionsByValue/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int sum (int a, int b) {
+    int result = a + b;
+    std::cout << "In: &result(int): " << &result << std::endl;
+    return result;
+}
+
+std::string add_string(std::string str1, std::string str2) {
+    std::string result = str1 + str2;
+    std::cout << "In: &result(std::string): " << &result << std::endl;
+    
+    return result;
+}
+
+int main()
+{   
+
+ /*   
+    int x{5};
+    int y{9};
+
+    int result = sum(x, y);
+    std::cout << "Out: &result(int): " << &result << std::endl;
+*/
+
+    std::string in_str1{"Hello"};
+    std::string in_str2{"World"};
+    std::string result_str = add_string(in_str1, in_str2);
+    std::cout << "Out: &result_str(string): " << &result_str << std::endl;
+    std::cout <<"result: " << result_str << std::endl;
+
+    return 0;
+}
