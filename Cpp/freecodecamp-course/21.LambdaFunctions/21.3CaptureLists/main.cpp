995cc14929bb04f59593f8f919b6e092501e8bd8
Albert Santos <alberteinsteincds@gmail.com>
2022-03-23 13:59:57 -0300

    capture Lists

--- Cpp/freecodecamp-course/21.LambdaFunctions/21.3CaptureLists/main.cpp
+++ Cpp/freecodecamp-course/21.LambdaFunctions/21.3CaptureLists/main.cpp
@@ -4,6 +4,7 @@ int main() {
     
     //capture lists
 
+/*
     double a{10};
     double b {20};
 
@@ -12,6 +13,37 @@ int main() {
     };
 
     func();
+*/
+
+/*
+    // Capturando pelo valor
+    // Cria uma cópia do está sendo captura
+    int c{42};
+    auto func = [c](){
+        std::cout << "Valor dentro da função lambda: " << c << ". &dentro: " << &c << std::endl;
+    };
+
+    for (size_t i{}; i<5; ++i){
+        std::cout << "Valor fora da função lambda: " << c << ". &fora: " << &c << std::endl;
+        func();
+        ++c; // incrementa o valor de c que está fora da função lambda
+    }
+*/
+
+
+
+    //Capturando por referência
+    // Não cria uma cópia. Qualquer alteração dentro da função lambda afeta diretamente a variável capturada
+    int c{42};
+    auto func = [&c](){
+        std::cout << "Valor dentro da função lambda: " << c << ". &dentro: " << &c << std::endl;
+    };
+
+    for (size_t i{}; i<5; ++i){
+        std::cout << "Valor fora da função lambda: " << c << ". &fora: " << &c << std::endl;
+        func();
+        ++c; // incrementa o valor de c que está fora da função lambda
+    }
 
     return 0;
 }
