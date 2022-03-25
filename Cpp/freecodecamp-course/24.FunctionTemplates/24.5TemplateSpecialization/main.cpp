e9acd0ac5ad0d743a3631c7e04a6420c14952fff
Albert Santos <alberteinsteincds@gmail.com>
2022-03-25 01:06:51 -0300

    template specialization pt2

--- Cpp/freecodecamp-course/24.FunctionTemplates/24.5TemplateSpecialization/main.cpp
+++ Cpp/freecodecamp-course/24.FunctionTemplates/24.5TemplateSpecialization/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 template <typename T> T maximum(T a, T b) {
     return (a>b)? a : b;
@@ -6,7 +7,7 @@ template <typename T> T maximum(T a, T b) {
 
 template <>
 const char* maximum<const char*>(const char* a, const char* b){
-    
+    return (std::strcmp(a, b) > 0) ? a : b;
 }
 
 int main() {
