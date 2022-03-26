873d5bd394d9eca141158272948f03cf76282621
Albert Santos <alberteinsteincds@gmail.com>
2022-03-26 19:08:43 -0300

    default constructors

---
+++ Cpp/freecodecamp-course/26.Classes/26.4DeafaultConstructors/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+const double PI{3.14159265};
+
+class Cylinder {
+    public:
+
+        // Constructor
+        Cylinder() = default;
+        Cylinder(double rad_param, double height_param){
+            base_radius = rad_param;
+            height = height_param;
+        }
+
+        // Métodos (Funções)
+        double volume() {
+            return PI * base_radius * base_radius * height;
+        }
+
+    private:
+        // Member variables
+        double base_radius{1};
+        double height{1};
+};
+
+int main()
+{   
+
+    Cylinder cylinder1; // Objeto instanciado a partir da classe
+    std::cout << "volume: " << cylinder1.volume() << std::endl;
+    
+    // std::cout << "base_radius: " << cylinder1.base_radius << std::endl;
+    // std::cout << "height: " << cylinder1.height << std::endl;
+    return 0;
+}
