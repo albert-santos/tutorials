20406e3f725df8b356c576fdd035c429cca45c25
Albert Santos <alberteinsteincds@gmail.com>
2022-04-05 16:43:08 -0300

    different levels of polimorphism pt1

---
+++ Cpp/freecodecamp-course/37.Polymorphism/37.8PolymorphismAtDifferentLevels/cat.h
@@ -0,0 +1,28 @@
+#ifndef CAT_H
+#define CAT_H
+
+#include "feline.h"
+
+class Cat: public Feline
+{
+    Cat() = default;
+    Cat(std::string_view fur_style, std::string_view description);
+    virtual ~Cat();
+
+    virtual void miaw() const {
+        std::cout << "Cat::miaw() called for: " << m_description << std::endl;
+    }
+
+    virtual void breathe() const {
+        std::cout << "Cat::breathe called for: " << m_description <<std::endl;
+    }
+
+    virtual void run() const {
+        std::cout << "Cat " << m_description << " is running" << std::endl;
+    }
+};
+
+
+
+#endif
+
